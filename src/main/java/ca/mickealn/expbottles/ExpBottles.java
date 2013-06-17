de2b8a5d91ec21d4077a395cb021a0b6b0ce5865
Mickeal Nevakshonoff <mikenevak@gmail.com>
2013-06-16 22:37:11 -0500

    Code Cleanup and restructuring

--- src/main/java/ca/mickealn/expbottles/ExpBottles.java
+++ src/main/java/ca/mickealn/expbottles/ExpBottles.java
@@ -1,16 +1,15 @@
 package ca.mickealn.expbottles;
 
 import org.bukkit.Bukkit;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class ExpBottles extends JavaPlugin implements Listener {
+import ca.mickealn.expbottles.ExpBottlesListener;
 
-    OnEnchantTableEvent enchantListener = new OnEnchantTableEvent();
-    ExpBottleBreakListener bottleListener = new ExpBottleBreakListener();
+public final class ExpBottles extends JavaPlugin {
+
+    ExpBottlesListener enchantListener = new ExpBottlesListener();
 
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(enchantListener, this);
-        Bukkit.getPluginManager().registerEvents(bottleListener, this);
     }
 }
