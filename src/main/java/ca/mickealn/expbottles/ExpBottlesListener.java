64e9d6d3044b31ae4fc7e9976b7c9e3dc628a249
Mickeal Nevakshonoff <mikenevak@gmail.com>
2013-06-19 02:41:04 -0500

    Functionality revised. Now plugin fills as many bottles as possible, at most being the complete stack in the players hand.

--- src/main/java/ca/mickealn/expbottles/ExpBottlesListener.java
+++ src/main/java/ca/mickealn/expbottles/ExpBottlesListener.java
@@ -1,7 +1,5 @@
 package ca.mickealn.expbottles;
 
-import java.util.HashMap;
-
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -11,52 +9,39 @@ import org.bukkit.event.entity.ExpBottleEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 
 public final class ExpBottlesListener implements Listener {
+    public final int expPerBottle = 17;
 
-    @SuppressWarnings("deprecation")
     @EventHandler
     public void onOpenEnhantTable(InventoryOpenEvent event) {
         Player player = (Player) event.getPlayer();
         if (event.getInventory().getType() == InventoryType.ENCHANTING && (player.getItemInHand().getType() == Material.GLASS_BOTTLE)) {
             event.setCancelled(true);
             ItemStack stack = player.getItemInHand();
-            int playerNewExp = player.getTotalExperience() - 17; // Getting the new experience level to give back to the player.
-            PlayerInventory playerInv = player.getInventory();
-
-            if (player.getTotalExperience() >= 17) {
-                if (stack.getAmount() > 1) {
-                    stack.setAmount(stack.getAmount() - 1);
-                }
-                else {
-                    playerInv.removeItem(stack);
-                    player.updateInventory();
-                }
+            int originalExp = player.getTotalExperience();
+            final int amountOfBottles = player.getTotalExperience() / expPerBottle;
+            if (player.getLevel() < 1) {
+                player.sendMessage(ChatColor.DARK_RED + "You do not have enough EXP!");
+            } else if (amountOfBottles <= stack.getAmount()) { // If they have more bottles then they can create into EXP bottles and need to get some back
+                int playerNewExp = player.getTotalExperience() % expPerBottle; // Getting the new experience level to give back to the player.
                 player.setLevel(0); // Set everything to 0 to prevent doubling of EXP
-                player.setExp(0);
                 player.setTotalExperience(0);
                 player.giveExp(playerNewExp);
-                HashMap<Integer, ItemStack> map = playerInv.addItem(new ItemStack(Material.EXP_BOTTLE, 1));// Return XP if inventory was full
-                player.updateInventory();
-                if (!map.isEmpty()) {
-                    player.sendMessage(ChatColor.DARK_RED + "You did not have enough inventory space to bottle the experience.");
-                    player.giveExp(17);
-                    stack.setAmount(stack.getAmount() + 1);
-                    player.updateInventory();
+                player.setItemInHand(new ItemStack(Material.EXP_BOTTLE, amountOfBottles));
+                player.getWorld().dropItem(player.getLocation(), new ItemStack(Material.GLASS_BOTTLE, (stack.getAmount() - amountOfBottles)));
 
-                }
+            } else { // if they have more EXP/expPerBottle than bottles
+                player.setItemInHand(new ItemStack(Material.EXP_BOTTLE, stack.getAmount()));
+                player.setLevel(0); // Set everything to 0 to prevent doubling of EXP
+                player.setTotalExperience(0);
+                player.giveExp(originalExp - (stack.getAmount() * expPerBottle));
             }
-            else {
-                player.sendMessage(ChatColor.DARK_RED + "You do not have enough EXP!");
-
-            }
-
         }
     }
 
     @EventHandler
     public void onExpEvent(ExpBottleEvent event) {
-        event.setExperience(17);
+        event.setExperience(expPerBottle);
     }
 }
