e10b1210f3cf9dbd85ae8877d3877dc4b5c17101
Andy Shulman <andy.shulman@hotmail.com>
2013-06-20 15:07:34 -0400

    Fix inconsistant XP returning to player after bottling

--- src/main/java/ca/mickealn/expbottles/events/ExpBottlesListener.java
+++ src/main/java/ca/mickealn/expbottles/events/ExpBottlesListener.java
@@ -31,8 +31,25 @@ public final class ExpBottlesListener implements Listener {
                 player.setLevel(0); // Set everything to 0 to prevent doubling of EXP
                 player.setTotalExperience(0);
 
+                int newTotalExperience = originalExp - numEnchantedBottles * expPerBottle;
+                double newLevel, experienceToCurrentLevel;
+
+                if (newTotalExperience >= 887) {
+                    newLevel = (303 + Math.sqrt(56 * newTotalExperience - 32511)) / 14;
+                    experienceToCurrentLevel = 3.5 * newLevel * newLevel - 151.5 * newLevel + 2220;
+                } else if (newTotalExperience >= 292) {
+                    newLevel = (59 + Math.sqrt(24 * newTotalExperience - 5159)) / 6;
+                    experienceToCurrentLevel = 1.5 * newLevel * newLevel - 29.5 * newLevel + 360;
+                } else {
+                    newLevel = newTotalExperience / 17;
+                    experienceToCurrentLevel = newLevel * 17;
+                }
+
+                player.setTotalExperience(newTotalExperience);
+                player.setLevel((int) newLevel);
+                player.setExp((float) ((newTotalExperience - experienceToCurrentLevel) / player.getExpToLevel()));
+
                 player.setItemInHand(new ItemStack(Material.EXP_BOTTLE, numEnchantedBottles));
-                player.giveExp(Math.max(0, originalExp - numEnchantedBottles * expPerBottle));
                 if (bottleCount > numEnchantedBottles) {
                     player.getWorld().dropItem(player.getLocation(), new ItemStack(Material.GLASS_BOTTLE, bottleCount - numEnchantedBottles));
                 }
