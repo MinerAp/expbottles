c26abb566f4e4a7f7c3fe3922647db4537e80fa7
Andy Shulman <andy.shulman@hotmail.com>
2013-06-20 01:55:44 -0400

    Fix conditional

--- src/main/java/ca/mickealn/expbottles/ExpBottlesListener.java
+++ src/main/java/ca/mickealn/expbottles/ExpBottlesListener.java
@@ -17,16 +17,17 @@ public final class ExpBottlesListener implements Listener {
         if (event.getPlayer() instanceof Player && InventoryType.ENCHANTING.equals(event.getInventory().getType()) && Material.GLASS_BOTTLE.equals(event.getPlayer().getItemInHand().getType())) {
             Player player = (Player) event.getPlayer();
             int originalExp = player.getTotalExperience();
-            int numEnchantedBottles = Math.min(originalExp / expPerBottle, player.getItemInHand().getAmount());
-            int stackSize = player.getItemInHand().getAmount();
+            int bottleCount = player.getItemInHand().getAmount();
+            int numEnchantedBottles = Math.min(originalExp / expPerBottle, bottleCount);
 
             if (numEnchantedBottles > 0) {
                 player.setLevel(0); // Set everything to 0 to prevent doubling of EXP
                 player.setTotalExperience(0);
+
                 player.setItemInHand(new ItemStack(Material.EXP_BOTTLE, numEnchantedBottles));
                 player.giveExp(Math.max(0, originalExp - numEnchantedBottles * expPerBottle));
-                if (numEnchantedBottles > 0) {
-                    player.getWorld().dropItem(player.getLocation(), new ItemStack(Material.GLASS_BOTTLE, stackSize - numEnchantedBottles));
+                if (bottleCount > numEnchantedBottles) {
+                    player.getWorld().dropItem(player.getLocation(), new ItemStack(Material.GLASS_BOTTLE, bottleCount - numEnchantedBottles));
                 }
             }
 
